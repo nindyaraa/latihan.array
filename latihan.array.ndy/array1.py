b6b3c64b813564bd99b9e9ec80a35680cd12ab09
nindyaraa <nindya.rahma148@smk.belajar.id>
2024-11-04 15:16:27 +0700

    Add files via upload

---
+++ latihan.array.ndy/array1.py
@@ -0,0 +1,20 @@
+
+# Deklrasi array untuk menyimpan angka
+angka = []
+
+# Meminta input pengguna
+for i in range(5):
+    elemen = int(input(f"Masukan angka ke-{i+1}: "))
+    angka.append(elemen)
+
+# Menghitung frekuensi kemunculan 
+frekuensi = {}
+for elemen in angka:
+    if elemen in  frekuensi:
+        frekuensi[elemen] += 1
+else:
+    frekuensi[elemen] =1
+
+# Menampilkan hasil 
+for elemen, jumlah in frekuensi in frekuensi.items():
+    print(f"Angka {elemen} muncul sebanyak {jumlah} kali.")   
